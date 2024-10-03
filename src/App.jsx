2f1cbce6dab89e7c913b3e20c6a4ec7291793704
GregoireGeoffroy <thegathering92@gmail.com>
2024-10-03 14:22:29 +0300

    Hero & Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App() {
     return (
         <div>
             <Navbar />
+            <Card />
         </div>
     )
 }
