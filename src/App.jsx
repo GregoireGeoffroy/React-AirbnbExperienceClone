012d9b72f4e39415ffa3cfbdf0a89251a446db16
GregoireGeoffroy <thegathering92@gmail.com>
2024-10-03 14:51:12 +0300

    sold out text

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import data from "./data"
 export default function App() {
   const cards = data.map(item => {
       return (
-          <Card
-              key={item.id}
-              img={item.coverImg}
-              rating={item.stats.rating}
-              reviewCount={item.stats.reviewCount}
-              location={item.location}
-              title={item.title}
-              price={item.price}
-          />
+        <Card
+          key={item.id}
+          img={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          location={item.location}
+          title={item.title}
+          price={item.price}
+          openSpots={item.openSpots}
+        />
       )
   })        
   
